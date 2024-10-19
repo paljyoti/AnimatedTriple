520cbae613055b33f86ae889c73379b41d082811
DrivingAwareness <drivingawareness01@gmail.com>
2024-10-19 17:22:18 +0530

    add images

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import Loader from "./Loader";
 import videoSrc from "../../src/assets/blackNwhite.mp4";
 import image from "../../src/assets/business-5475661_1280.jpg";
+import image1 from "../../src/assets/social-media-4140959_1280.jpg"
 import image2 from "../../src/assets/content-marketing-4111003_1280.jpg";
 import image3 from "../../src/assets/digital-7971754_1280.jpg";
+import image4 from "../../src/assets/woman-1446557_1280.jpg";
+import image5 from "../../src/assets/business-5475659_1280.jpg"
 
 function Blog() {
   return (
@@ -17,14 +20,12 @@ function Blog() {
             Insights, thoughts, industry trends, marketing tips, eDesign news,
             nerdy stuff, it's all here.
           </p>
-          <h1 className="mt-10">
-            tips, eDesign news,nerdy stuff, it's all here.
-          </h1>
+          <p className="pt-10 text-3xl font-bold">TECHNOLOGY TRENDS</p>
           <img src={image} alt="" className="mt-5" />
 
 
           <p className="pt-32 text-3xl font-bold">TECHNOLOGY TRENDS</p>
-          <img src={image} alt="" className="mt-5 mb-10" />
+          <img src={image1} alt="" className="mt-5 mb-10" />
         </div>
         <div className="img">
           <p className="pt-60 text-3xl font-bold">TECHNOLOGY TRENDS</p>
@@ -32,7 +33,7 @@ function Blog() {
 
 
           <p className="pt-32 text-3xl font-bold">TECHNOLOGY TRENDS</p>
-          <img src={image2} alt="" className="mt-5" />
+          <img src={image4} alt="" className="mt-5" />
         </div>
         <div className="img">
           <p className="pt-28 text-3xl font-bold">TECHNOLOGY TRENDS</p>
@@ -40,7 +41,7 @@ function Blog() {
 
 
           <p className="pt-28 text-3xl font-bold">TECHNOLOGY TRENDS</p>
-          <img src={image3} alt="" className="mt-5" />
+          <img src={image5} alt="" className="mt-5" />
         </div>
       </div>
       <div className="bg-yellow-400 h-96">
